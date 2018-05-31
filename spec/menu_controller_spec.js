eeab8808df8816a2957713dce502036c1d83206f
Mariel <mariel.capulli@gmail.com>
2018-05-31 11:24:10 -0400

    testing errors

---
+++ spec/menu_controller_spec.js
@@ -0,0 +1,9 @@
+describe("MenuController", () => {
+    describe("#getContactCount()", () => {
+
+        it("should return 0 when no contacts are in the book", () => {
+            const menu = new MenuController();
+            expect(menu.getContactCount()).toBe(0);
+        });
+    });
+})
